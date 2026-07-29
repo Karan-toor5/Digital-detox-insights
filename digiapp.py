c9142aa4cba3abff82673f253bf9cbfdccdc879e
Karan-toor5 <karandeep8706@gmail.com>
2026-07-29 17:07:17 +0530

    Update digiapp.py

--- digiapp.py
+++ digiapp.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import streamlit as st
-from ptl import image
-icon_image=image.open("1785324228193.png")
+#from ptl import image
+#icon_image=image.open("1785324228193.png")
 st.set_page_config(
     page_title="Digital Detox",
-    page_icon="icon_image",
+    page_icon="./1785324228193.png",
     layout="wide",
 )
 st.title("Digital Detox: Intelligent Insights for Digital Well-being")   
