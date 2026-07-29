bc0bcb4d338b2a8336c6f08ab0165db51222c864
Karan-toor5 <karandeep8706@gmail.com>
2026-07-29 17:05:17 +0530

    Change page icon to use file path
    Updated page icon path for Streamlit app.

--- digiapp.py
+++ digiapp.py
@@ -5,10 +5,10 @@ import seaborn as sns
 import plotly.express as px
 import streamlit as st
 from PTL import Image
-icon_image=Image.open("1785324228193.png")
+#icon_image=Image.open("1785324228193.png")
 st.set_page_config(
     page_title="Digital Detox",
-    page_icon="icon_image",
+    page_icon="./1785324228193.png",
     layout="wide",
 )
 st.title("Digital Detox: Intelligent Insights for Digital Well-being")   
