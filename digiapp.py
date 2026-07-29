585e01ceede1600a142a18d9aa92ba90d4dd358d
Karan-toor5 <karandeep8706@gmail.com>
2026-07-29 17:05:43 +0530

    Fix import case for PTL Image module

--- digiapp.py
+++ digiapp.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import streamlit as st
-from PTL import Image
+from PTL import image
 #icon_image=Image.open("1785324228193.png")
 st.set_page_config(
     page_title="Digital Detox",
